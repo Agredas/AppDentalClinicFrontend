0d8ea8e52400a9dc42aa6647af096fbb07809e0f
Agredas <andreagredas@gmail.com>
2020-10-20 19:13:40 +0200

    Added Home and Login and Sass installed.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, BrowserRouter, Route} from 'react-router-dom'
 import './App.css';
+
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Home from './containers/Home/Home';
+import Login from './containeres/Login/Login';
+
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
         <Switch>
-
+          <Route path='/' component={Home} exact/>
+          <Route path='/login' component={Login} exact/>
         </Switch>
       <Footer />
     </BrowserRouter>
