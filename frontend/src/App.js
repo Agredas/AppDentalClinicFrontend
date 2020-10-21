ff14078b4b6ce9bb489f56b1092b5bc09ec81617
Agredas <andreagredas@gmail.com>
2020-10-21 23:05:28 +0200

    Trying register.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, BrowserRouter, Route} from 'react-router-dom'
-import './App.css';
+import './App.scss';
 
 
 import Footer from './components/Footer/Footer';
@@ -8,7 +8,7 @@ import Header from './components/Header/Header';
 
 
 import Home from './containers/Home/Home';
-/* import Register from './containers/Register/Register'; */
+import Register from './containers/Register/Register';
 import Login from './containers/Login/Login';
 /* import Logout from './containers/Logout/Logout';
 import Modify from './containers/Modify/Modify';
@@ -21,7 +21,7 @@ function App() {
       <Header />
         <Switch>
           <Route path='/' component={Home} exact/>
-          {/* <Route path='/register' component={Register} exact/> */}
+          <Route path='/register' component={Register} exact/>
           <Route path='/login' component={Login} exact/>
 
           {/* <Route path='/logout' component={Logout} exact/>
--- frontend/src/containers/Register/Register.jsx
+++ frontend/src/containers/Register/Register.jsx
@@ -1,33 +1,123 @@
-import React from 'react';
-import axions from 'axios';
 import './Register.scss'
+import React from 'react';
+import { Form, Input, Button, notification } from 'antd';
+import axios from 'axios';
+const layout = {
+    labelCol: {
+        span: 8,
+    },
+    wrapperCol: {
+        span: 16,
+    },
+};
+const tailLayout = {
+    wrapperCol: {
+        offset: 8,
+        span: 16,
+    },
+};
 
 const Register = () =>{
-  const handleSubmit = event =>{
-    event.preventDefault();
-    const client ={
-      name:event.target.name.value,
-      surnames:event.target.surnames.value,
-      phone:event.target.phone.value,
-      email:event.target.email.value,
-      password:event.target.password.value
-    };
-    axios.post('http://localhost:3001/client/register', client)
-    .then(res => {
-      console.log(res)
-    })
-    .catch(error => console.log (error.response.data))
-  }
-  return (
-    <form className="register-form" onSubmit={handleSubmit}>
-      <input type="name" name="name" required placeholder="Your name" />
-      <input type="surnames" name="surnames" required placeholder="Your surnames" />
-      <input type="phone" name="phone" required placeholder="Your phone" />
-      <input type="email" name="email" required placeholder="Your e-mail addres" />
-      <input type="password" name="password" required placeholder="Your password"/>
-      <button type="submit">Register</button>
-    </form>
-  )
+
+  const onFinish = (user) => {
+    axios.post('http://localhost:3001/users/register', user)
+        .then(res => {
+            console.log(res.data)
+            notification.success({ message :'Registered client.',description:'Succesfully registered client.'})
+        }).catch(error => {
+            notification.error({ message: 'Registration error.', description: 'There was an error trying to register the client.' })
+        })
+};
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+    return (
+      <Form
+              {...layout}
+              name="basic"
+              initialValues={{
+                  remember: true,
+              }}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+          >
+              <Form.Item
+                  label="Name"
+                  name="name"
+                  rules={[
+                      {
+                          required: true,
+                          message: 'Please, enter your name.',
+                      },
+                  ]}
+              >
+                  <Input />
+              </Form.Item>
+              <Form.Item
+                  label="Surnames"
+                  name="surnames"
+                  rules={[
+                      {
+                          required: true,
+                          message: 'Please, enter your surnames.',
+                      },
+                  ]}
+              >
+                  <Input />
+              </Form.Item>
+              <Form.Item
+                  label="Phone"
+                  name="phone"
+                  rules={[
+                      {
+                          required: true,
+                          message: 'Please, enter your phone number.',
+                      },
+                  ]}
+              >
+                  <Input.Password />
+              </Form.Item>
+              <Form.Item
+                  label="Email"
+                  name="email"
+                  rules={[
+                      {
+                          required: true,
+                          message: 'Please, enter your email.',
+                      },
+                      {
+                        type:'email',
+                        message: 'The field must be an email.',
+                    }
+                  ]}
+              >
+                  <Input />
+              </Form.Item>
+              <Form.Item
+                  label="Password"
+                  name="password"
+                  rules={[
+                      {
+                          required: true,
+                          message: 'Please, enter your password.',
+                      },
+                      {
+                          pattern:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/, 
+                          message: 'Your password must contain at least one lower case, one upper case, one number, one special character, and must be between 8 and 10 characters long!',
+                      }
+                  ]}
+              >
+                  <Input.Password />
+              </Form.Item>
+
+              <Form.Item {...tailLayout}>
+                  <Button type="primary" htmlType="submit">
+                      Submit
+          </Button>
+              </Form.Item>
+          </Form>
+    )
 }
 
 export default Register;
