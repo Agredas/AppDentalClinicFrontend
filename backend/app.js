df1e6cef7bb476f523a5684ed0fa648243e7d2d7
Agredas <andreagredas@gmail.com>
2020-10-23 18:27:08 +0200

    Cors backend changed.

--- backend/app.js
+++ backend/app.js
@@ -9,8 +9,16 @@ const AppointmentsRouter = require('./routers/appointmentRouter');
 const PORT = process.env.PORT || 3001;
 //Middleware
 app.use(express.json());
-app.use(cors);
+/* app.use(cors);
+ */
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 // DB Connection
 const dbconnect = require('./config/dbconnect');
@@ -21,3 +29,5 @@ app.use('/appointment', auth, AppointmentsRouter);
 
 // Port Listen
 app.listen(PORT, () => console.log('Server running on port ' + PORT + '.'))
+
+
