c9197cc873303ff31f141472476510b3115dede7
Agredas <andreagredas@gmail.com>
2020-10-23 17:21:05 +0200

    Login fixed but need design changes.

--- frontend/src/containers/Login/Login.jsx
+++ frontend/src/containers/Login/Login.jsx
@@ -1,33 +1,34 @@
 import React from 'react';
 import axios from 'axios';
 import './Login.scss';
-
 import {useHistory} from 'react-router';
+import {notification} from 'antd'
+
 const Login = ({setClient}) => {
   const history = useHistory();
   const handleSubmit = event =>{
-    event.preventDefault(); // Prevent the page from refreshing.
-    const bodyClient = {
-      email:event.target.email.value,
-      password:event.target.password.value
-    };    
-    axios.post('http://localhost:3001/client/login', bodyClient)
-    .then(res => {
-      console.log(res.data.client);
-      setClient(res.data.bodyClient)
-      localStorage.setItem('authToken', JSON.stringify(res.data.token))
-      localStorage.setItem('client', JSON.stringify(res.data.client))
-
-      setTimeout(() => {
-        history.push('/')
-      }, 1500);
-    }).catch (error => console.log(error.response.data))
-  }
+    event.preventDefault(); // to prevent refreshing the page
+    const client ={
+        email:event.target.email.value,
+        password:event.target.password.value
+    };
+    axios.post(process.env.REACT_APP_BASE_URL+'/client/login', client)
+    .then(res=>{
+        setClient(res.data.client) 
+        localStorage.setItem('authToken',res.data.token);
+        localStorage.setItem('client',JSON.stringify(res.data.client))
+        notification.success({message:'Welcome!',description:'Welcome! '+client.email})
+        setTimeout(() => {
+            history.push('/')
+        }, 1000);
+    })
+    .catch(error=>console.log(error))
+}
   return (
     <form className='login-form' onSubmit={handleSubmit}>
       <input type='email' name='email' required placeholder='Write your email'/>
       <input type="password" name="password" required placeholder="Introduce tu contraseña"/>
-      <button type="submit">Log in</button>
+      <button type="submit">Login</button>
     </form>
   )
 }
