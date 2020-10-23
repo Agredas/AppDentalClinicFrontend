8dfc08f7436545c5870f1d5fd973b0332334db23
Agredas <andreagredas@gmail.com>
2020-10-23 17:21:15 +0200

    Register fixed but need design changes.

--- frontend/src/containers/Register/Register.jsx
+++ frontend/src/containers/Register/Register.jsx
@@ -1,24 +1,8 @@
 import React from 'react';
 import {useHistory} from "react-router";
 
-import { Form, Input, Button, notification } from 'antd';
+import {Input, Button, notification } from 'antd';
 import axios from 'axios';
-
-const layout = {
-    labelCol: {
-        span: 8,
-    },
-    wrapperCol: {
-        span: 7,
-    },
-};
-const tailLayout = {
-    wrapperCol: {
-        offset: 11,
-        span: 8,
-    },
-};
-
 
 const Register = () =>{
 
@@ -47,89 +31,18 @@ const Register = () =>{
     }
 
     return (
-      <Form onSubmit={handleSubmit}
-              {...layout}
-              name="basic"
-              initialValues={{
-                  remember: true,
-              }}
-          >
-              <Form.Item
-                  label="Name"
-                  name="name"
-                  rules={[
-                      {
-                          required: true,
-                          message: 'Please, enter your name.',
-                      },
-                  ]}
-              >
-                  <Input />
-              </Form.Item>
-              <Form.Item
-                  label="Surnames"
-                  name="surnames"
-                  rules={[
-                      {
-                          required: true,
-                          message: 'Please, enter your surnames.',
-                      },
-                  ]}
-              >
-                  <Input />
-              </Form.Item>
-              <Form.Item
-                  label="Phone"
-                  name="phone"
-                  rules={[
-                      {
-                          required: true,
-                          message: 'Please, enter your phone number.',
-                      },
-                  ]}
-              >
-                  <Input />
-              </Form.Item>
-              <Form.Item
-                  label="Email"
-                  name="email"
-                  rules={[
-                      {
-                          required: true,
-                          message: 'Please, enter your email.',
-                      },
-                      {
-                        type:'email',
-                        pattern:/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/,
-                        message: 'Your email must contain @ and finish with .com or .es.',
-                    }
-                  ]}
-              >
-                  <Input />
-              </Form.Item>
-              <Form.Item
-                  label="Password"
-                  name="password"
-                  rules={[
-                      {
-                          required: true,
-                          message: 'Please, enter your password.',
-                      },
-                      {
-                          pattern:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/, 
-                          message: 'Your password must contain at least one lower case, one upper case, one number, one special character, and must be between 8 and 10 characters long!',
-                      }
-                  ]}
-              >
-                  <Input.Password />
-              </Form.Item>
+        <form className="register-form" onSubmit={handleSubmit}>
 
-              <Form.Item {...tailLayout}>
-                  <Button>Submit</Button>
-              </Form.Item>
-          </Form>
-          
-    )
+        <Input type="name" name="name" required placeholder="Write your name" />
+        <Input type="surnames" name="surnames" required placeholder="Write your surnames" />
+        <Input type="phone" name="phone" required placeholder="Write your phone" />
+        <Input type="email" name="email" required placeholder="Write your email" />
+        <Input type="password" name="password" required placeholder="Write your password" />
+
+        <Button type="primary" htmlType="submit">Sign up</Button>
+
+    </form>
+)
 }
 
 export default Register;
