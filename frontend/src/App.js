d52d351c733af2ac3cf91eb0adb0d772e41fa837
Agredas <andreagredas@gmail.com>
2020-10-23 12:38:29 +0200

    Added token to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,13 +10,27 @@ import Header from './components/Header/Header';
 import Home from './containers/Home/Home';
 import Register from './containers/Register/Register';
 import Login from './containers/Login/Login';
+import axios from 'axios';
 
 
 
 function App() {
+
+  const [client, setClient] = usestate(JSON.parse(localStorage.getItem('client')));
+  useEffect(() =>{
+    const token = localStorage.getItem('authToken')
+    axios.get(process.env.REACT_APP_BASE_URL + '/client/',{
+      headers:{
+        Authorization: token
+      }
+    })
+    .then(res => {
+      setClient(res.data)
+    })
+  }, [])
   return (
     <BrowserRouter>
-      <Header />
+      <Header client={client} setClient={setClient}/>
         <Switch>
           <Route path='/' component={Home} exact/>
           <Route path='/register' component={Register} exact/>
