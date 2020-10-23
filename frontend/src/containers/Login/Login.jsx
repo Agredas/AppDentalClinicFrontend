04f5f72c1b8418ba6173373abf6f9f9126b6e1b8
Agredas <andreagredas@gmail.com>
2020-10-23 12:50:07 +0200

    Added Token to Login.

--- frontend/src/containers/Login/Login.jsx
+++ frontend/src/containers/Login/Login.jsx
@@ -2,26 +2,32 @@ import React from 'react';
 import axios from 'axios';
 import './Login.scss';
 
-const Login = () => {
-  const handleSubmit = async (event) =>{
-    try{
-      event.preventDefault(); // Prevent the page from refreshing.
-      const client = {
+import {useHistory} from 'react-router';
+const Login = ({setClient}) => {
+  const history = useHistory();
+  const handleSubmit = event =>{
+    event.preventDefault(); // Prevent the page from refreshing.
+    const bodyClient = {
       email:event.target.email.value,
       password:event.target.password.value
-      };    
-      let result = await axios.post('http://localhost:3001/client/login', client)
-      let token = result.data.token;
-      localStorage.setItem("token", token);
-    }catch (error){
-      console.log(error)
-    }
+    };    
+    axios.post('http://localhost:3001/client/login', bodyClient)
+    .then(res => {
+      console.log(res.data.client);
+
+      localStorage.setItem('authToken', JSON.stringify(res.data.token))
+      localStorage.setItem('client', JSON.stringify(res.data.client))
+
+      setTimeout(() => {
+        history.push('/')
+      }, 1500);
+    }).catch (error => console.log(error.response.data))
   }
   return (
-    <form className="login-form" onSubmit={handleSubmit}>
-      <input type="email" name="email" required placeholder="Your e-mail address" />
-      <input type="password" name="password" required placeholder="Your password"/>
-      <button type="submit">Log In</button>
+    <form className='login-form' onSubmit={handleSubmit}>
+      <input type='email' name='email' required placeholder='Write your email'/>
+      <input type="password" name="password" required placeholder="Introduce tu contraseña"/>
+      <button type="submit">Log in</button>
     </form>
   )
 }
