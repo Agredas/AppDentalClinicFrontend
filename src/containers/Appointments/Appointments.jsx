f75a02f7376f2b4f88c5a894ef237f938953934c
Agredas <andreagredas@gmail.com>
2020-10-30 19:05:20 +0100

    Deleting Appointments fixed.

--- src/containers/Appointments/Appointments.jsx
+++ src/containers/Appointments/Appointments.jsx
@@ -1,8 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios'
 import './Appointments.scss';
+import {notification} from 'antd';
+import {useHistory} from 'react-router';
 
 const Appointments = () =>{
+  const history = useHistory();
   const [appointments,setAppointments] = useState([]);
   const token = localStorage.getItem('authToken')
   useEffect(()=>{
@@ -19,14 +22,19 @@ const Appointments = () =>{
   const deleteAppointment = async (id) =>{
     const options = { headers: { Authorization: `Bearer ${token}` }};
     await axios.delete('http://localhost:3001/appointment/cancel/' + id, options)
-
+    notification.success({message:'Appointment has been successfully cancelled.', description:'Appointment has been successfully cancelled.'})
+    await axios.get('http://localhost:3001/appointment/show', options)
+    .then((res) =>{
+      console.log(res.data)
+      setAppointments(res.data.appointment);
+    }).catch((error) =>{
+      console.log(error);
+    })
   }
-
-
   return (
         <div className="appointmentContainer">
             {appointments?.map(appointment =>
-                <div className="cardAppointment">{appointment.title}<button onClick={()=> {deleteAppointment(appointment._id)}}>X</button>
+                <div key={appointment._id} className="cardAppointment">{appointment.title}<button onClick={()=> {deleteAppointment(appointment._id)}}>X</button>
                 </div>
             )}
         </div>
